aca5dbd2c38960327fee14d138334eb83cefb4ce
yorkinbek <baqoyevy@gmail.com>
2023-01-10 16:49:28 +0500

    create migration configuration inside main.go file

--- Dockerfile
+++ Dockerfile
@@ -4,20 +4,16 @@ WORKDIR /app
 COPY . .
 
 RUN go build -o main main.go
-RUN apk add curl
-RUN  curl -L https://github.com/golang-migrate/migrate/releases/download/v4.14.1/migrate.linux-amd64.tar.gz | tar xvz
-
 
 
 FROM alpine:3.17
 WORKDIR /app
 
 COPY --from=builder /app/main .
-COPY --from=builder /app/migrate.linux-amd64 ./migrate
 COPY app.env .
 COPY start.sh .
 COPY wait-for.sh .
-COPY db/migration ./migration
+COPY db/migration ./db/migration
 
 EXPOSE 8080
 CMD ["/app/main"]
--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -7,6 +7,8 @@ import (
 	db "github.com/yorqinbek/simple_bank/db/sqlc"
 	pb "github.com/yorqinbek/simple_bank/proto-gen"
 	"github.com/yorqinbek/simple_bank/util"
+	"github.com/yorqinbek/simple_bank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -14,7 +16,10 @@ import (
 
 // LoginUser ...
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
-
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
 	user, err := server.store.GetUser(ctx, req.Username)
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -70,4 +75,16 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiredAt),
 	}
 	return rsp, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	return violations
 }
